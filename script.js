6169a7ed8c97b6e4e19fafc3c0451492eb41aa93
kakaora <rohin.moza@gmail.com.com>
2021-11-30 14:59:26 -0700

    commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-//EASY WAY
 //var myName = window.prompt("What's your name?");
 //console.log("Hello",myName);
 
-//HARD WAY
 document.getElementById("myButton").onclick = function(){
 
     var myName = document.getElementById("myText").value;
